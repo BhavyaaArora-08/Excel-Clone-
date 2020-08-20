66aea08f85aaa963291a466b4a537be413cf6cd7
Bhavyaa Arora <44175278+Bhavyaa08@users.noreply.github.com>
2020-08-20 06:35:34 +0530

    Breakpoint 2

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const electron = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const ejs = require("ejs-electron");
+ejs.data({
+  title: "Excel Clone",
+  rows: 100,
+  cols: 26,
+});
 
 const createWindow = () => {
   // provides node to electron app
   const win = new BrowserWindow({ webPreferences: { nodeIntegration: true } });
-  win.loadFile("index.html").then(() => {
+  win.loadFile("index.ejs").then(() => {
     win.maximize();
     win.webContents.openDevTools();
   });
